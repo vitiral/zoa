ea672463ded965deedcf24a3dec1192373692c9c
Rett Berg <rett@google.com>
2022-01-12 16:05:09 -0600

    add San implementation

--- TestTamu.py
+++ TestTamu.py
@@ -1,9 +1,46 @@
+import io
 import unittest
 from tamu import *
 
-class TestPy(unittest.TestCase):
+def assert_roundtrip(v):
+  sab = Sab.from_(v)
+  b = sab.serialize()
+  result_sab = from_sab(b)
+  result = result_sab.to_py()
+  assert v == result
+
+class TestSan(unittest.TestCase):
   def test_pass(self):
     pass
+
+  def test_write_str(self):
+    b = io.BytesIO()
+    write_data(b, b'hi')
+    b = b.getvalue()
+    assert b[0] == 2
+    assert b[1:] == b'hi'
+
+  def test_write_arr_str(self):
+    bw = io.BytesIO()
+    v = [Sab.new_data(b'hi')]
+    assert v[0].data == b'hi'
+    write_arr(bw, v)
+    b = bw.getvalue()
+    print(b)
+    assert b[0] == SAB_ARR | 1
+    assert b[1] == 2 # the string
+    assert b[2:] == b'hi'
+
+  def test_from_arr_str(self):
+    v = from_sab(io.BytesIO(b'\x02hi'))
+    assert v == Sab.new_data(b'hi')
+
+  def test_from_to(self):
+    assert_roundtrip([])
+    assert_roundtrip([b'hi', b'bob'])
+    assert_roundtrip([ [] ])
+    assert_roundtrip([b'hi', [] ])
+    assert_roundtrip([b'hi', [b'bob']])
 
 if __name__ == '__main__':
   unittest.main()
--- tamu.py
+++ tamu.py
@@ -3,24 +3,31 @@ import unittest
 
 from dataclasses import dataclass
 
-TAMU_LEN_MASK = 0x3F
-TAMU_JOIN = 0x40
-TAMU_ARR = 0x80
+SAB_LEN_MASK = 0x3F
+SAB_JOIN = 0x40
+SAB_ARR = 0x80
 
 def isbytes(v): return isinstance(v, (bytes, bytearray))
 
 @dataclass
-class San(object):
+class Sab(object):
   data: bytearray
-  arr: list["San"]
+  arr: list["Sab"]
 
   @classmethod
   def from_(cls, value):
     if isbytes(value): return cls.new_data(value)
     out = []
     for v in value:
-      out.append(San.from_(v))
+      out.append(Sab.from_(v))
     return cls.new_arr(out)
+
+  def to_py(self):
+    if self.data: return self.data
+    out = []
+    for v in self.arr:
+      out.append(v.to_py())
+    return out
 
   @classmethod
   def new_arr(cls, value=None):
@@ -29,6 +36,13 @@ class San(object):
   @classmethod
   def new_data(cls, value=None):
     return cls(data=value if value is not None else bytearray(), arr=None)
+
+  def serialize(self, bw=None):
+    bw = bw if bw is not None else io.BytesIO()
+    if self.data: write_data(bw, self.data)
+    else:         write_arr(bw, self.arr)
+    bw.seek(0)
+    return bw
 
   def extend(self, value):
     if isbytes(value):
@@ -43,53 +57,52 @@ class San(object):
       return self.data
     return self.arr
 
-  def serialize(self, bw: io.BytesIO = None):
-    bw = bw if bw is not None else io.BytesIO()
-    if self.data is not None:
-      bw.write(self.datA)
-      return to
+def int_from_bytes(b: bytes):
+  return int.from_bytes(b, 'big')
+
+def write_byte(bw: io.BytesIO, v: int):
+  return bw.write(v.to_bytes(1, 'big'))
 
 def write_data(bw: io.BytesIO, data: bytes, join=False):
+  assert join == 0 or join == SAB_JOIN
   if join: assert len(arr) <= 64
-  join = TAMU_JOIN if join else 0
   if len(data) == 0:
     assert not join
-    bw.write(0) # No join bit, arr bit, or length
+    bw.write(b'\0') # No join bit, arr bit, or length
     return
 
   # write any join blocks
   i = 0
   while len(data) - i > 64:
-    b.write(TAMU_JOIN | 64)
+    write_byte(bw, SAB_JOIN | 64)
     bw.write(bw, data[i:i+64])
     i += 64
-  bw.write(len(data) - i) # note: not joined
-  bw.write(bw, data[i:])
+  write_byte(bw, len(data) - i) # note: not joined
+  bw.write(data[i:])
 
-def write_arr(bw: io.BytesIO, arr: list[San], join=False):
+def write_arr(bw: io.BytesIO, arr: list[Sab], join=False):
+  assert join == 0 or join == SAB_JOIN
   if join: assert len(arr) <= 64
   if len(arr) == 0:
-    assert not join
-    bw.write(TAMU_ARR)
+    write_byte(bw, SAB_ARR)
     return
 
-  join = TAMU_JOIN if join else 0
-  bw.write(TAMU_ARR | join | min(64, len(arr)))
+  write_byte(bw, SAB_ARR | join | min(64, len(arr)))
 
   # write out the first 64 values
   i = 0
-  while i < 64:
+  while i < 64 and i < len(arr):
     v = arr[i]
-    if v.data: write_data(bw, v.data)
-    else: write_arr(bw, v.arr)
+    if v.data  is not None:  write_data(bw, v.data)
+    elif v.arr is not None: write_arr(bw, v.arr)
+    else: raise ValueError(v)
     i += 1
-  if not i: return
+  if i == len(arr): return
 
   # write out the remaining values in 64 block chunks
   while len(arr) - i > 0:
     write_arr(bw, arr[i:i+64], join=True)
     i += min(64, len(arr) - i)
-
 
 def readexact(br: io.BytesIO, to: bytearray, length: int):
   while length:
@@ -97,25 +110,27 @@ def readexact(br: io.BytesIO, to: bytearray, length: int):
     length -= len(got)
     to.extend(got)
 
+def from_sab(br: io.BytesIO, num:int = 1, joinTo:Sab = None):
+  if isinstance(br, bytes): br = io.BytesIo(br)
 
-def from_san(br: io.BytesIO, num:int = 1, joinTo:San = None):
-  ty = br.read(1)
+  ty = int_from_bytes(br.read(1))
   if joinTo:          out = joinTo
-  elif TAMU_ARR & ty: out = San.new_arr()
-  else:               out = San.newData()
-  length = TAMU_LEN_MASK & ty
+  elif SAB_ARR & ty:  out = Sab.new_arr()
+  else:               out = Sab.new_data()
+  length = SAB_LEN_MASK & ty
 
-  if not (TAMU_ARR & ty):
+  if not (SAB_ARR & ty): # if not arr
     if out.data is None: raise ValueError("invalid join")
     readexact(br, out.data, length)
-    if TAMU_JOIN & ty:
-      san(br, joinTo=out)
+    if SAB_JOIN & ty:
+      from_sab(br, joinTo=out)
     return out
 
+  # arr
   if out.arr is None: raise ValueError("invalid join")
   for _ in range(length):
-    out.arr.append(san(br))
+    out.arr.append(from_sab(br))
 
-  if TAMU_JOIN & ty:
-    san(br, num=1, joinTo=out)
-
+  if SAB_JOIN & ty:
+    from_sab(br, num=1, joinTo=out)
+  return out
